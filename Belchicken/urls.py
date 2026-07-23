6bb118826bf71fd9f6e50c8b6ea05f1c17602957
Kevin Bougouma <bclaudekevin@gmail.vom>
2026-07-23 18:11:25 +0000

    Ajout

--- Belchicken/urls.py
+++ Belchicken/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.models import User
 
-if not User.objects.filter(username="Claude").exists():
-    User.objects.create_superuser("Claude", "bclaudekevin@gmail.com", "Kevin6059")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
